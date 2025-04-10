fdffed5533869c6a4f66be8a43c25a120860bb88
Alex Marrero <alexmarrero0215@gmail.com>
2025-04-10 04:57:27 -0400

    Added GraphQL mutations for login, signup, saveBook, and removeBook

--- Develop/client/src/utils/queries.ts
+++ Develop/client/src/utils/queries.ts
@@ -17,3 +17,63 @@ export const QUERY_ME = gql`
     }
   }
 `;
+
+export const QUERY_ALL_USERS = gql`
+  query GetAllUsers {
+    users {
+      _id
+      username
+      email
+    }
+  }
+`;
+
+export const QUERY_USER_BY_ID = gql`
+  query GetUserById($userId: ID!) {
+    user(userId: $userId) {
+      _id
+      username
+      email
+      savedBooks {
+        bookId
+        title
+      }
+    }
+  }
+`;
+
+export const QUERY_BOOK_BY_ID = gql`
+  query GetBookById($bookId: ID!) {
+    book(bookId: $bookId) {
+      bookId
+      title
+      authors
+      description
+      image
+      link
+    }
+  }
+`;
+
+export const QUERY_SAVED_BOOKS = gql`
+  query GetSavedBooks {
+    savedBooks {
+      bookId
+      title
+      authors
+      description
+      image
+      link
+    }
+  }
+`;
+
+export const QUERY_USER_STATS = gql`
+  query GetUserStats {
+    userStats {
+      totalSavedBooks
+      recentActivity
+    }
+  }
+`;
+
