cec20c006fc27374a43709319efea247cfa1403a
Alex Marrero <alexmarrero0215@gmail.com>
2025-04-10 04:55:43 -0400

    Removed deprecated REST functions from API.ts; retained searchGoogleBooks

--- Develop/client/src/utils/API.ts
+++ Develop/client/src/utils/API.ts
@@ -1,17 +1,17 @@
-import type { User } from '../models/User.js';
-import type { Book } from '../models/Book.js';
+// import type { User } from '../models/User';
+// import type { Book } from '../models/Book';
 
 // route to get logged in user's info (needs the token)
-export const getMe = (token: string) => {
+/* export const getMe = (token: string) => {
   return fetch('/api/users/me', {
     headers: {
       'Content-Type': 'application/json',
       authorization: `Bearer ${token}`,
     },
   });
-};
+}; */
 
-export const createUser = (userData: User) => {
+/* export const createUser = (userData: User) => {
   return fetch('/api/users', {
     method: 'POST',
     headers: {
@@ -19,9 +19,9 @@ export const createUser = (userData: User) => {
     },
     body: JSON.stringify(userData),
   });
-};
+}; */
 
-export const loginUser = (userData: User) => {
+/* export const loginUser = (userData: User) => {
   return fetch('/api/users/login', {
     method: 'POST',
     headers: {
@@ -29,10 +29,10 @@ export const loginUser = (userData: User) => {
     },
     body: JSON.stringify(userData),
   });
-};
+}; */
 
 // save book data for a logged in user
-export const saveBook = (bookData: Book, token: string) => {
+/* export const saveBook = (bookData: Book, token: string) => {
   return fetch('/api/users', {
     method: 'PUT',
     headers: {
@@ -41,17 +41,17 @@ export const saveBook = (bookData: Book, token: string) => {
     },
     body: JSON.stringify(bookData),
   });
-};
+}; */
 
 // remove saved book data for a logged in user
-export const deleteBook = (bookId: string, token: string) => {
+/* export const deleteBook = (bookId: string, token: string) => {
   return fetch(`/api/users/books/${bookId}`, {
     method: 'DELETE',
     headers: {
       authorization: `Bearer ${token}`,
     },
   });
-};
+}; */
 
 // make a search to google books api
 // https://www.googleapis.com/books/v1/volumes?q=harry+potter
